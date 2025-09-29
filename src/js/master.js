181222f3b6402d6c396618a5e13875c47fcc3255
parsa <parsafarashah@gmail.com>
2025-09-29 17:16:30 +0330

    add shadow on text with parallax

--- src/js/master.js
+++ src/js/master.js
@@ -10,3 +10,39 @@ frBtn.addEventListener("click", () => {
   en.classList.toggle("border");
   en.classList.toggle("overflow-hidden");
 });
+
+let vhSite = window.innerHeight;
+console.log(vhSite);
+let shadowText = document.getElementById("shadowText");
+// section have scroll
+let secScroll = document.querySelector("#main");
+let textOnMouse = document.querySelector("#textOnMouse");
+secScroll.addEventListener("scroll", () => {
+  let st = parseInt(secScroll.scrollTop);
+  if (st > vhSite / 10) {
+    shadowText.classList.remove("hidden");
+    shadowText.classList.add("flex");
+    // for text on mouse
+    textOnMouse.classList.remove("opacity-100");
+    textOnMouse.classList.add("opacity-40");
+  } else {
+    shadowText.classList.add("hidden");
+    shadowText.classList.remove("flex");
+    // for text on mouse
+    textOnMouse.classList.remove("opacity-40");
+    textOnMouse.classList.add("opacity-100");
+  }
+  if (st > vhSite / 3) {
+    shadowText.classList.remove("opacity-50");
+    shadowText.classList.add("opacity-80");
+    // for text on mouse
+    textOnMouse.classList.remove("opacity-40");
+    textOnMouse.classList.add("opacity-20");
+  } else {
+    shadowText.classList.add("opacity-50");
+    shadowText.classList.remove("opacity-80");
+    // for text on mouse
+    textOnMouse.classList.add("opacity-40");
+    textOnMouse.classList.remove("opacity-20");
+  }
+});
